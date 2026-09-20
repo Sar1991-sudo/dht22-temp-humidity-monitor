d1ec59aaf909128611176c8541bc40b5c8a22999
sarmad al-obaidi <sarmad.shatti@gmail.com>
2026-09-20 11:46:58 +0300

    First commit - IoT DHT22 project

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include "DHT.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
+String apiKey = "1B828SSD119W36CY";  
+const char* server = "http://api.thingspeak.com/update";
+
+void setup() {
+  Serial.begin(115200);
+  delay(3000);
+  Serial.println("Test");
+  dht.begin();
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to WiFi!");
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print(" C, Humidity: ");
+  Serial.print(hum);
+  Serial.println(" %");
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    String url = String(server) + "?api_key=" + apiKey + "&field1=" + String(temp) + "&field2=" + String(hum);
+
+    http.begin(url);
+    int httpCode = http.GET();
+
+    Serial.print("HTTP response code: ");
+    Serial.println(httpCode);
+
+    http.end();
+  }
+
+  delay(20000);  
+}
